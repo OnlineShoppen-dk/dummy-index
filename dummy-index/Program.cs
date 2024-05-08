eed12ae81d042270f0b0c9ba75b8bf1f2e5de10a
Rashaun Godding <90447626+rgodding@users.noreply.github.com>
2024-05-08 18:42:03 +0200

    changed Program.cs

--- dummy-index/Program.cs
+++ dummy-index/Program.cs
@@ -1,45 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-// Setup Connection to RabbitMQ
-
-using System.Text;
+﻿using System.Text;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
-var factory = new ConnectionFactory()
+namespace dummy_index
 {
-    HostName = "rabbitqueue",
-    UserName = "user",
-    Password = "userpass"
-};
-
-using var connection = factory.CreateConnection();
-using var channel = connection.CreateModel();
-
-// Declare Queue : Add Product
-ConsumeAddProductMessage(channel);
-
-Console.ReadLine();
-return;
-
-void ConsumeAddProductMessage(IModel channel1)
-{
-    var consumer = new EventingBasicConsumer(channel1);
-
-    channel1.QueueDeclare(queue: "productAddQueue",
-        durable: true,
-        exclusive: false,
-        autoDelete: false,
-        arguments: null);
-
-    Console.WriteLine(" [*] Waiting for messages.");
-    
-    
-    consumer.Received += (model, ea) =>
+    class Program
     {
-        var body = ea.Body.ToArray();
-        var message = Encoding.UTF8.GetString(body);
-        Console.WriteLine(" [x] Received {0}", message);
-    
-    };
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost", 
+                UserName = "user", 
+                Password = "userpass"
+            };
+
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "productQueue",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "productAddQueue",
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine("Enter to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
 }
