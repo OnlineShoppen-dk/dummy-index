a9af2aa807c62751f0b05f3a65408724dae8e119
Rashaun Godding <90447626+rgodding@users.noreply.github.com>
2024-05-08 19:09:38 +0200

    can add data to elastic search through productQueue

--- dummy-index/Program.cs
+++ dummy-index/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System.Text;
+using dummy_index.Models;
+using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
+using Newtonsoft.Json;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
@@ -10,7 +14,7 @@ namespace dummy_index
         {
             var factory = new ConnectionFactory()
             {
-                HostName = "rabbitqueue", 
+                HostName = "localhost", 
                 UserName = "user", 
                 Password = "userpass"
             };
@@ -31,6 +35,10 @@ namespace dummy_index
                     var body = ea.Body.ToArray();
                     var message = Encoding.UTF8.GetString(body);
                     Console.WriteLine(" [x] Received {0}", message);
+                    
+                    // Add the product to the index
+                    var product = JsonConvert.DeserializeObject<Product>(message);
+                    AddProductToIndex(product);
                 };
                 channel.BasicConsume(queue: "productQueue",
                     autoAck: true,
@@ -42,6 +50,47 @@ namespace dummy_index
                     // You can add any additional processing or sleeping logic here if needed
                 }
             }
+        }
+        public static async void AddProductToIndex(Product product)
+        {
+            // Setup Elasticsearch
+            const string userName = "elastic";
+            const string password = "changeme";
+            
+            var nodes = new Uri[]
+            {
+                new Uri("http://localhost:9200"),
+            };
+        
+            var pool = new StaticNodePool(nodes);
+
+
+            var settings = new ElasticsearchClientSettings(pool)
+                // Default Mapping
+                .DefaultMappingFor<Product>(m => m
+                    .IndexName("product-logs")
+                )
+                // Setup
+                .Authentication(new BasicAuthentication(userName, password))
+                .EnableDebugMode() // Optional: Enables detailed logging for debugging
+                .PrettyJson() // Optional: Formats JSON output to be more readable
+                .RequestTimeout(TimeSpan.FromMinutes(2));
+            
+
+            var client = new ElasticsearchClient(settings);
+            
+            var response = await client.IndexAsync(product);
+            
+            if (!response.IsValidResponse)
+            {
+                Console.WriteLine("Failed to add product");
+                Console.WriteLine(response.DebugInformation);
+            }
+            else
+            {
+                Console.WriteLine("Product added to index");
+            }
+        
         }
     }
 }
